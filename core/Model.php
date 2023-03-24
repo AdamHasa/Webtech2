05bac5bfbc945a3d1cffb618bebf51ecc8bc00e5
AntoniBayens <56294701+AntoniBayens@users.noreply.github.com>
2023-03-24 10:37:44 +0100

    auth controller bug fix

--- core/Model.php
+++ core/Model.php
@@ -35,25 +35,16 @@ abstract class Model
                 }
                 if ($ruleName === self::RULE_REQUIRED && !$value) {
                     $this->addError($attribute, self::RULE_REQUIRED);
-                }
-                if ($ruleName === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                    $this->addError($attribute, self::RULE_EMAIL);
-                }
-                if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
-                    $this->addError($attribute, self::RULE_MIN);
-                }
-                if ($ruleName === self::RULE_MAX && strlen($value) < $rule['max']) {
-                    $this->addError($attribute, self::RULE_MAX);
+
                 }
 
             }
-
-            return empty($this->errors);
-
         }
+
+        return empty($this->errors);
     }
 
-    public function addError(string $attribute, string $rule)
+    public function addError(string $attribute, string $rule): void
     {
         $message = $this->errorMessages()[$rule] ?? '';
         $this->errors[$attribute][] = $message;
